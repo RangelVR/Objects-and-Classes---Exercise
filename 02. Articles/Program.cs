655d5db38b0c66427b12f7f9b23908b0d81fe4a2
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-12-04 21:42:38 +0200

    Update Program.cs

--- 02. Articles/Program.cs
+++ 02. Articles/Program.cs
@@ -71,3 +71,79 @@ namespace _02.Articles
         }
     }
 }
+======================================================================================
+using System;
+using System.Linq;
+using System.Text;
+
+namespace _02.Articles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ").ToArray();
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            Article newArticle = new Article(input[0], input[1], input[2]);
+
+            while (counter != n)
+            {
+                string[] command = Console.ReadLine().Split(": ").ToArray();
+
+                if (command[0] == "Edit")
+                {
+                    newArticle.Edit(command[1]);
+                }
+                else if (command[0] == "ChangeAuthor")
+                {
+                    newArticle.ChangeAuthor(command[1]);
+                }
+                else if (command[0] == "Rename")
+                {
+                    newArticle.Rename(command[1]);
+                }
+                counter++;
+            }
+            Console.WriteLine(newArticle);
+        } 
+        
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            Title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            //return $"{Title} - {Content}: {Author}";
+            StringBuilder sb = new StringBuilder($"{Title} - {Content}: {Author}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
