2f81713cc98c3a2726cb6286d0d31907c60524db
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-26 16:12:43 +0200

    Update Program.cs

--- 03. Articles 2.0/Program.cs
+++ 03. Articles 2.0/Program.cs
@@ -1,69 +1,61 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace _03.Articles_2_0
+int numArticles = int.Parse(Console.ReadLine());
+
+List<Article> articleList = new List<Article>();
+
+while (numArticles > 0)
 {
-    class Program
+    string[] articleInput = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+    string title = articleInput[0];
+    string content = articleInput[1];
+    string author = articleInput[2];
+
+    Article article = new Article(title, content, author);
+    articleList.Add(article);
+
+    numArticles--;
+}
+
+string command = Console.ReadLine();
+
+if (command == "title")
+{
+    articleList = articleList.OrderBy(x => x.Title).ToList();
+}
+else if (command == "content")
+{
+    articleList = articleList.OrderBy(x => x.Content).ToList();
+}
+else if (command == "author")
+{
+    articleList = articleList.OrderBy(x => x.Author).ToList();
+
+}
+
+foreach (Article article in articleList)
+{
+    Console.WriteLine(article);
+}
+
+
+class Article
+{
+    public Article(string title, string content, string author)
     {
-        static void Main(string[] args)
-        {
-            int n = int.Parse(Console.ReadLine());
-
-            List<Article> catalog = new List<Article>();
-
-            for (int i = 0; i < n; i++)
-            {
-                string[] input = Console.ReadLine().Split(", ").ToArray();
-                Article newArticle = new Article()
-                {
-                    Title = input[0],
-                    Content = input[1],
-                    Author = input[2],
-                };
-
-                catalog.Add(newArticle);
-            }
-
-            string command = Console.ReadLine();
-
-            if (command == "title")
-            {
-                catalog = catalog.OrderBy(x => x.Title).ToList();
-            }
-            else if (command == "content")
-            {
-                catalog = catalog.OrderBy(x => x.Content).ToList();
-            }
-            else if (command == "author")
-            {
-               catalog = catalog.OrderBy(x => x.Author).ToList();
-            }
-
-            //Console.WriteLine(string.Join(Environment.NewLine, catalog));
-
-            foreach (Article article in catalog)
-            {
-                Console.WriteLine(article);
-            }
-        } 
-        
+        Title = title;
+        Content = content;
+        Author = author;
     }
 
-    class Article
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public string Author { get; set; }
+
+    public override string ToString()
     {
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public string Author { get; set; }
-
-        public override string ToString()
-        {
-            //return $"{Title} - {Content}: {Author}";
-            StringBuilder sb = new StringBuilder($"{Title} - {Content}: {Author}");
-            return sb.ToString().TrimEnd();
-        }
+        return $"{Title} - {Content}: {Author}";
     }
-
-    
 }
